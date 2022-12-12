c1f02e27ecbe45e6b09c47b2c7772cb174f4c25f
Alexandru Bagu <alexandrubagu87@gmail.com>
2022-12-12 09:22:43 +0200

    init project

---
+++ lib/flow_csv_ex.ex
@@ -0,0 +1,18 @@
+defmodule FlowCsvEx do
+  @moduledoc """
+  Documentation for `FlowCsvEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlowCsvEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flow_csv_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule FlowCsvExTest do
+  use ExUnit.Case
+  doctest FlowCsvEx
+
+  test "greets the world" do
+    assert FlowCsvEx.hello() == :world
+  end
+end
